4de58169f5f96e41ab95357f40ba2b3f57b173f9
Paulo Ferreira <phgferreira@outlook.com>
2022-09-07 20:11:33 -0300

    Exibindo a primeira aula

--- src/br/com/alura/TestandoListas.java
+++ src/br/com/alura/TestandoListas.java
@@ -23,5 +23,8 @@ public class TestandoListas {
 		for (String aula : aulas) {
 			System.out.println("Aula: " + aula);
 		}
+		
+		String primeiraAula = aulas.get(0);
+		System.out.println("A primeira aula é " + primeiraAula);
 	}
 }
