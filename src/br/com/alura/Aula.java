ce1e3ef9714f0d9aa35c13a35e043b6307990a63
Paulo Ferreira <phgferreira@outlook.com>
2022-09-07 20:58:22 -0300

    Usando o toString que o próprio eclipse fornece

--- src/br/com/alura/Aula.java
+++ src/br/com/alura/Aula.java
@@ -20,7 +20,7 @@ public class Aula implements Comparable<Aula> {
 	
 	@Override
 	public String toString() {
-		return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
+		return "Aula [titulo=" + titulo + ", tempo=" + tempo + "]";
 	}
 
 	@Override
