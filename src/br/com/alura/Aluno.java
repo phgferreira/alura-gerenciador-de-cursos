60cbea901f9b6b4c4bb067b8716e7a157e9bea06
Paulo Ferreira <phgferreira@outlook.com>
2022-09-08 12:25:12 -0300

    Criando a classe Alunoe e um TesteAlunoComCurso

---
+++ src/br/com/alura/Aluno.java
@@ -0,0 +1,21 @@
+package br.com.alura;
+
+public class Aluno {
+
+	private String nome;
+	private Integer numeroMatricula;
+
+	public Aluno(String nome, Integer numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Integer getNumeroMatricula() {
+		return numeroMatricula;
+	}
+
+}
