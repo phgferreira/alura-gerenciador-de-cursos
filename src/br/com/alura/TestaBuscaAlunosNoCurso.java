9da89ab413c80ea86ac191748c9c89ad55159bf6
Paulo Ferreira <phgferreira@outlook.com>
2022-09-08 20:24:33 -0300

    Preparando para uso do mapa

---
+++ src/br/com/alura/TestaBuscaAlunosNoCurso.java
@@ -0,0 +1,20 @@
+package br.com.alura;
+
+public class TestaBuscaAlunosNoCurso {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+		
+		Aluno rodrigo = new Aluno("Rodrigo Turini", 34672);
+		Aluno guilherme = new Aluno("Guilherme Silveira", 5617);;
+		Aluno mauricio = new Aluno("Mauricio Aniche", 17645);
+		
+		javaColecoes.matricula(rodrigo);
+		javaColecoes.matricula(guilherme);
+		javaColecoes.matricula(mauricio);
+	}
+
+}
