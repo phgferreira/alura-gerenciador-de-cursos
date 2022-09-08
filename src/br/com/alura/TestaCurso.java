ece852fc1c4eafc211fedd64d0cbcd5fc15bce55
Paulo Ferreira <phgferreira@outlook.com>
2022-09-07 21:15:28 -0300

    Demonstando que ao adicionar um curso na referencia aulas, já estamos adicionando no objeto aulas do curso

--- src/br/com/alura/TestaCurso.java
+++ src/br/com/alura/TestaCurso.java
@@ -8,5 +8,8 @@ public class TestaCurso {
 		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
 		List<Aula> aulas = javaColecoes.getAulas();
 		System.out.println(aulas);
+		
+		aulas.add(new Aula("Trabalhando com ArrayList", 21));
+		System.out.println(javaColecoes.getAulas());
 	}
 }
