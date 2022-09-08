c9b4824a7ebc0e7080b0d5021c127f6bea2552ba
Paulo Ferreira <phgferreira@outlook.com>
2022-09-08 07:41:25 -0300

    Criando uma coleção de alunos

---
+++ src/br/com/alura/TestaAluno.java
@@ -0,0 +1,28 @@
+package br.com.alura;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAluno {
+
+	public static void main(String[] args) {
+		Set<String> alunos = new HashSet<String>();
+		alunos.add("Rodrigo Turini");
+		alunos.add("Alberto Souza");
+		alunos.add("Nico Steppat");
+		alunos.add("Sérgio Lopes");
+		alunos.add("Renan Saggio");
+		alunos.add("Mauricio Aniche");
+		
+		/* 1ª regra de usar um Set ou uma Collections é que não
+		 * tem garantia de que a ordem continuará da mesma forma
+		 * que foi inserida
+		 */
+		System.out.println(alunos);
+		
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+	}
+
+}
