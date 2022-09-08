f3a540898e07249857afaaf92e72b7ad3f661f01
Paulo Ferreira <phgferreira@outlook.com>
2022-09-07 21:44:26 -0300

    Criando uma cópia da lista de aulas e ordenando-a

---
+++ src/br/com/alura/TestaCurso2.java
@@ -0,0 +1,23 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso2 {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
+		
+		// Dessa forma nós instanciamos um novo objeto lista recebendo a lista imutável, é feito uma cópia
+		List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+		Collections.sort(aulas);
+		System.out.println(aulas);
+	}
+}
