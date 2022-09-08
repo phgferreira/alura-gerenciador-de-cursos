df00dff9a05b06a6dd2dbf1bab74a0a143f238c0
Paulo Ferreira <phgferreira@outlook.com>
2022-09-07 21:55:09 -0300

    Criando um toString para o Curso

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -28,6 +28,11 @@ public class Curso {
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
 	}
 
+	@Override
+	public String toString() {
+		return "Curso [nome=" + nome + ", tempo total=" + getTempoTotal() + "]";
+	}
+
 	public String getNome() {
 		return nome;
 	}
--- src/br/com/alura/TestaCurso2.java
+++ src/br/com/alura/TestaCurso2.java
@@ -21,5 +21,7 @@ public class TestaCurso2 {
 		System.out.println(aulas);
 		
 		System.out.println(javaColecoes.getTempoTotal());
+		
+		System.out.println(javaColecoes);
 	}
 }
